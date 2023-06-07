625c8442bbe4e263c331d94b4003c19644a38a2d
RishiYadav66 <rishiyadav2910@gmail.com>
2023-06-07 08:44:48 +0530

    domain

--- backend/server.js
+++ backend/server.js
@@ -7,68 +7,56 @@ const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
-
-//db connection
-mongoose.connect("mongodb://localhost:27017/stack-overflow", { useNewUrlParser: true, useUnifiedTopology: true });
-
+// DB connection
+mongoose.connect("mongodb+srv://rishiyadav2910:rishiyadav4966@cluster0.jr1edb1.mongodb.net/stack-overflow?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "stack-overflow",
+});
 const db = mongoose.connection;
-
 db.once("open", () => {
     try
     {
-
-        console.log("Mongodb connection established")
-    }
-    catch (e)
+        console.log("MongoDB connection established");
+    } catch (e)
     {
-        console.log('error:', e)
+        console.log('Error:', e);
     }
-})
+});
 
-
-
-
-//middleware 
-app.use(bodyParser.json({ limit: "55mb" }))
-app.use(bodyParser.urlencoded({ extended: true, limit: "55mb" }))
+// Middleware
+app.use(bodyParser.json({ limit: "15mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "15mb" }));
 app.use(express.json());
 
-
-//cors header methods
+// CORS headers
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://dev-central.netlify.app');
     res.header('Access-Control-Allow-Headers', '*');
     next();
-})
+});
 
-
-//api
-
+// API
 app.use('/api', router);
 
-//static resources
-
-// app.use('/upload', express.static(path.join(__dirname, "/../uploads")))
-app.use(express.static(path.join(__dirname, "/../frontend/dist")))
+// Static resources
+app.use(express.static(path.join(__dirname, "/../frontend/dist")));
 
 app.get('*', (req, res) => {
-
     try
     {
-
-        res.sendFile(path.join(`${__dirname}/../frontend/dist/index.html`))
-    }
-    catch (e)
+        res.sendFile(path.join(`${__dirname}/../frontend/dist/index.html`));
+    } catch (e)
     {
-        res.send("Error Occured")
+        res.send("Error Occurred");
     }
-})
-
-//cors
-app.use(cors())
+});
 
 
-//server listen
+// CORS
+app.use(cors());
+
+// Server listen
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
